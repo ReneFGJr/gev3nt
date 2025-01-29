5cd3e78fa66dbc244e2e6de8456ab0b186492cba
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-29 18:21:06 -0300

    Update main.php

--- v2/app/Views/main.php
+++ v2/app/Views/main.php
@@ -1,8 +1,19 @@
-<div class="container">
-	<div class="row">
-		<div class="col-12 col-md-8 offset-md-2 border border-3 border-secondary rounded-5 bg-white mt-5 p-4">
-			<?php echo $navbar; ?>
-			<?php echo $event; ?>
+<div class="container py-5">
+	<div class="row justify-content-center">
+		<div class="col-lg-10 col-md-12 border border-3 border-secondary rounded-4 bg-white shadow-lg p-4">
+			<!-- Navbar -->
+			<div class="mb-4">
+				<?php echo $navbar; ?>
+			</div>
+
+			<!-- Conteúdo do Evento -->
+			<div class="p-3 fs-5 fs-md-4 fs-lg-3"> <!-- Fonte maior para mobile -->
+				<?php echo $event; ?>
+			</div>
 		</div>
 	</div>
 </div>
+
+<style>
+
+</style>

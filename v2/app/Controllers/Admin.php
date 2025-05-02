9c73711f22dd225346db1b6e857d96b3215a84a9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-05-02 18:58:09 -0300

    Update Admin.php

--- v2/app/Controllers/Admin.php
+++ v2/app/Controllers/Admin.php
@@ -53,60 +53,61 @@ class Admin extends BaseController
 					$Publications = new \App\Models\OJS\Publications();
 					$data['event'] .= $Publications->work($a2, $ev);
 					$ApiOJS = new \App\Models\OJS\Api();
-					if (get("update") != '')
-					{
+					if (get("update") != '') {
 						$dt = $ApiOJS->updateDB($a2);
-						$redirect = '<script>window.location.href ="/admin/work/' . $a2.'";</script>';
+						$redirect = '<script>window.location.href ="/admin/work/' . $a2 . '";</script>';
 						return $redirect;
 					}
 					break;
 				/**********************	Inscições  */
 				case 'inscricoes':
-					break;
-				case 'validar':
-					$dt['inscricoes'] = $EventInscritos->getInscritos($ide);
-					$data['event'] = view('admin/inscricoes_validar', $dt);
-					break;
-				case 'check':
-					$dt['data'] = $EventInscritos->getInscricao($a3);
-					$dt['action'] = true;
-					$data['event'] = view('admin/inscricao', $dt);
-					break;
-				case 'view':
-					$dt['data'] = $EventInscritos->getInscricao($a3);
-					$dt['action'] = false;
-					$data['event'] = view('admin/inscricao', $dt);
-					break;
+					switch ($a2) {
+						case 'validar':
+							$dt = [];
+							$dt['inscricoes'] = $EventInscritos->getInscritos($ide);
+							$data['event'] = view('admin/inscricoes_validar', $dt);
+							break;
+						case 'check':
+							$dt['data'] = $EventInscritos->getInscricao($a3);
+							$dt['action'] = true;
+							$data['event'] = view('admin/inscricao', $dt);
+							break;
+						case 'view':
+							$dt['data'] = $EventInscritos->getInscricao($a3);
+							$dt['action'] = false;
+							$data['event'] = view('admin/inscricao', $dt);
+							break;
 
-				case 'checked':
-					$dt['action'] = false;
-					$dt['data'] = $EventInscritos->getInscricao($a3);
-					$data['event'] .= view('admin/inscricao', $dt);
+						case 'checked':
+							$dt['action'] = false;
+							$dt['data'] = $EventInscritos->getInscricao($a3);
+							$data['event'] .= view('admin/inscricao', $dt);
 
-					if ($dt['data']['ein_recibo'] == '') {
-						/********************************** Acoes */
-						/********************************** Enviar e-mail */
-						$EmailX = new \App\Models\IO\EmailX();
-						$email = $dt['data']['n_email'];
-						$txto = $EventInscritos->messages(1, $dt['data']);
-						$rsp = $EmailX->sendEmail($email, 'Confirmação de pagamento', $txto);
+							if ($dt['data']['ein_recibo'] == '') {
+								/********************************** Acoes */
+								/********************************** Enviar e-mail */
+								$EmailX = new \App\Models\IO\EmailX();
+								$email = $dt['data']['n_email'];
+								$txto = $EventInscritos->messages(1, $dt['data']);
+								$rsp = $EmailX->sendEmail($email, 'Confirmação de pagamento', $txto);
 
 
-						/*********************************** Atualiza banco de Dados */
-						$EventInscritos->where('id_ein', $a3)
-							->set('ein_recibo', 1)
-							->set('ein_pago', date("Y-m-d H:i:s"))
-							->set('ein_pago', 1)
-							->update();
-						$data['event'] .= '<li>' . $rsp . '</li>';
-						$data['event'] .= '<li>Inscrição confirmada</li>';
-					} else {
-						$data['event'] .= '<li>Validação de pagamento já realizada</li>';
+								/*********************************** Atualiza banco de Dados */
+								$EventInscritos->where('id_ein', $a3)
+									->set('ein_recibo', 1)
+									->set('ein_pago', date("Y-m-d H:i:s"))
+									->set('ein_pago', 1)
+									->update();
+								$data['event'] .= '<li>' . $rsp . '</li>';
+								$data['event'] .= '<li>Inscrição confirmada</li>';
+							} else {
+								$data['event'] .= '<li>Validação de pagamento já realizada</li>';
+							}
+							break;
+						default:
+							$data['event'] .= view('admin/main', $data);
+							break;
 					}
-					break;
-				default:
-					$data['event'] .= view('admin/main', $data);
-					break;
 			}
 		}
 

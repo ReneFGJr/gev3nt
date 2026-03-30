820f1dcf0ae5fdad26994f3081011306c0aabd94
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-03-29 23:10:01 -0300

    Update index.php

--- app/Views/layout/index.php
+++ app/Views/layout/index.php
@@ -8,8 +8,6 @@
     <div class="col-md-12">
         <div class="card shadow-lg border-0" style="background: rgba(44,83,100,0.15); border-radius: 16px;">
             <div class="card-body p-2 text-center">
-                <h1 class="mb-4" style="color:#90caf9; font-weight:700;">Bem-vindo ao Gev3nt</h1>
-                <p class="lead mb-4" style="color:#f4f4f4;">Gerencie inscrições, eventos e participantes de forma simples, moderna e segura.</p>
                 <?php if (!session('usuario')): ?>
                     <div class="d-grid gap-3 col-8 mx-auto mt-4">
                         <a href="<?= base_url('auth/login') ?>" class="btn btn-primary btn-lg fw-bold" style="background:#1976d2; border:none;">Login</a>
